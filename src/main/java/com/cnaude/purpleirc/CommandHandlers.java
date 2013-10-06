d8733fae36796ec70b4787d59256a7e71876f164
cnaude <cnaude@wilde.ph.cox.net>
2013-10-06 11:34:18 -0700

    Refactoring

--- src/main/java/com/cnaude/purpleirc/CommandHandlers.java
+++ src/main/java/com/cnaude/purpleirc/CommandHandlers.java
@@ -1,6 +1,9 @@
 package com.cnaude.purpleirc;
 
 import com.cnaude.purpleirc.Commands.*;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.HashMap;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,269 +14,71 @@ import org.bukkit.command.CommandSender;
  */
 public class CommandHandlers implements CommandExecutor {
 
+    HashMap<String, Object> commands = new HashMap<String, Object>();
     private PurpleIRC plugin;
-    private final AddOp addOp;
-    private final Connect connect;
-    private final CTCP ctcp;
-    private final DeOp deOp;
-    private final Debug debug;
-    private final Disconnect disconnect;
-    private final Join join;
-    private final Kick kick;
-    private final Leave leave;
-    private final List list;
-    private final ListBots listBots;
-    private final ListOps listOps;
-    private final Login login;
-    private final MessageDelay messageDelay;
-    private final Msg msg;
-    private final Mute mute;
-    private final Nick nick;
-    private final Notice notice;
-    private final Op op;
-    private final Reload reload;
-    private final ReloadBot reloadBot;
-    private final ReloadBotConfig reloadBotConfig;
-    private final ReloadBotConfigs reloadBotConfigs;
-    private final ReloadBots reloadBots;
-    private final ReloadConfig reloadConfig;
-    private final RemoveOp removeOp;
-    private final Save save;
-    private final Say say;
-    private final Send send;
-    private final SendRaw sendRaw;
-    private final Server server;
-    private final Topic topic;
-    private final Whois whois;
 
     public CommandHandlers(PurpleIRC plugin) {
+
         this.plugin = plugin;
-        this.addOp = new AddOp(plugin);
-        this.connect = new Connect(plugin);
-        this.ctcp = new CTCP(plugin);
-        this.deOp = new DeOp(plugin);
-        this.debug = new Debug(plugin);
-        this.disconnect = new Disconnect(plugin);
-        this.join = new Join(plugin);
-        this.kick = new Kick(plugin);
-        this.leave = new Leave(plugin);
-        this.list = new List(plugin);
-        this.listBots = new ListBots(plugin);
-        this.listOps = new ListOps(plugin);
-        this.login = new Login(plugin);
-        this.messageDelay = new MessageDelay(plugin);
-        this.msg = new Msg(plugin);
-        this.mute = new Mute(plugin);
-        this.nick = new Nick(plugin);
-        this.notice = new Notice(plugin);
-        this.op = new Op(plugin);
-        this.reload = new Reload(plugin);
-        this.reloadBot = new ReloadBot(plugin);
-        this.reloadBotConfig = new ReloadBotConfig(plugin);
-        this.reloadBotConfigs = new ReloadBotConfigs(plugin);
-        this.reloadBots = new ReloadBots(plugin);
-        this.reloadConfig = new ReloadConfig(plugin);
-        this.removeOp = new RemoveOp(plugin);
-        this.save = new Save(plugin);
-        this.say = new Say(plugin);
-        this.send = new Send(plugin);
-        this.sendRaw = new SendRaw(plugin);
-        this.server = new Server(plugin);
-        this.topic = new Topic(plugin);
-        this.whois = new Whois(plugin);
+
+        commands.put("addop", new AddOp(plugin));
+        commands.put("connect", new Connect(plugin));
+        commands.put("ctcp", new CTCP(plugin));
+        commands.put("deop", new DeOp(plugin));
+        commands.put("debug", new Debug(plugin));
+        commands.put("disconnect", new Disconnect(plugin));
+        commands.put("join", new Join(plugin));
+        commands.put("kick", new Kick(plugin));
+        commands.put("leave", new Leave(plugin));
+        commands.put("list", new List(plugin));
+        commands.put("listbots", new ListBots(plugin));
+        commands.put("listops", new ListOps(plugin));
+        commands.put("login", new Login(plugin));
+        commands.put("messagedelay", new MessageDelay(plugin));
+        commands.put("msg", new Msg(plugin));
+        commands.put("mute", new Mute(plugin));
+        commands.put("nick", new Nick(plugin));
+        commands.put("notice", new Notice(plugin));
+        commands.put("op", new Op(plugin));
+        commands.put("reload", new Reload(plugin));
+        commands.put("reloadbot", new ReloadBot(plugin));
+        commands.put("reloadbotconfig", new ReloadBotConfig(plugin));
+        commands.put("reloadbotconfigs", new ReloadBotConfigs(plugin));
+        commands.put("reloadbots", new ReloadBots(plugin));
+        commands.put("reloadconfig", new ReloadConfig(plugin));
+        commands.put("removeop", new RemoveOp(plugin));
+        commands.put("save", new Save(plugin));
+        commands.put("say", new Say(plugin));
+        commands.put("send", new Send(plugin));
+        commands.put("sendraw", new SendRaw(plugin));
+        commands.put("server", new Server(plugin));
+        commands.put("topic", new Topic(plugin));
+        commands.put("whois", new Whois(plugin));
     }
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-        /*
-        String[] subArgs = null;
-        plugin.logDebug("ARGS: " + args.length);
-        if (args.length >= 2) {
-            subArgs = new String[args.length - 2];
-            for (int i = 2; i < args.length; i++) {
-                subArgs[i - 2] = args[i];
-                plugin.logDebug("a[" + i + "] " + args[i]);
-            }
-        } else if (args.length >= 1) {
-            subArgs = new String[1];
-                subArgs[i - 1] = args[i];
-                plugin.logDebug("a[" + i + "] " + args[i]);
-        } else {
-            subArgs = args;
-        }
-        for (String s1 : subArgs) {
-            plugin.logDebug(s1);
-        }*/
 
         if (args.length >= 1) {
-            String botName = "";
-
-            if (plugin.ircBots.size() == 1) {
-                botName = (String) plugin.ircBots.keySet().toArray()[0];
-            } else {
-                if (args.length >= 2) {
-                    for (String s : plugin.ircBots.keySet()) {
-                        if (s.equalsIgnoreCase(args[1])) {
-                            botName = s;
-                            break;
-                        }
-                    }
-                }
-            }
-
             String subCmd = args[0].toLowerCase();
             if (!sender.hasPermission("irc." + subCmd)) {
                 sender.sendMessage(plugin.noPermission);
                 return true;
             }
-            if (subCmd.equalsIgnoreCase("listbots")) {
-                listBots.dispatch(sender);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("debug")) {
-                debug.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("connect")) {
-                connect.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("reload")) {
-                reload.dispatch(sender);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("reloadbot")) {
-                reloadBot.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("reloadbots")) {
-                reloadBots.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("reloadbotconfig")) {
-                reloadBotConfig.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("reloadbotconfigs")) {
-                reloadBotConfigs.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("reloadconfig")) {
-                reloadConfig.dispatch(sender);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("disconnect")) {
-                disconnect.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("topic")) {
-                topic.dispatch(sender, args);
-                return true;
-            }            
-            if (subCmd.equalsIgnoreCase("msg")) {
-                msg.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("say")) {
-                say.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("send")) {
-                send.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("op")) {
-                op.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("deop")) {
-                deOp.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("kick")) {
-                kick.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("addop")) {
-                addOp.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("listops")) {
-                listOps.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("removeop")) {
-                removeOp.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("mute")) {
-                mute.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("save")) {
-                save.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("server")) {
-                server.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("nick")) {
-                nick.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("messagedelay")) {
-                messageDelay.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("login")) {
-                login.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("list")) {
-                list.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("whois")) {
-                whois.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("join")) {
-                join.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("leave")) {
-                leave.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("sendraw")) {
-                sendRaw.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("ctcp")) {
-                ctcp.dispatch(sender, args);
-                return true;
-            }
-            if (subCmd.equalsIgnoreCase("notice")) {
-                notice.dispatch(sender, args);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private String getBotName(String[] args) {
-        String botName = "";
-        if (plugin.ircBots.size() == 1) {
-            botName = (String) plugin.ircBots.keySet().toArray()[0];
-        } else {
-            for (String s : plugin.ircBots.keySet()) {
-                if (s.equalsIgnoreCase(args[1])) {
-                    botName = s;
-                    break;
+            if (commands.containsKey(subCmd)) {
+                try {
+                    Method method = commands.get(subCmd).getClass().getMethod("dispatch", CommandSender.class, String[].class);
+                    method.invoke(commands.get(subCmd), sender, args);
+                    return true;
+                } catch (NoSuchMethodException ex) {
+                    plugin.logError(ex.getMessage());
+                } catch (IllegalAccessException ex) {
+                    plugin.logError(ex.getMessage());
+                } catch (InvocationTargetException ex) {
+                    plugin.logError(ex.getMessage());
                 }
             }
         }
-        return botName;
+        return false;
     }
 }
